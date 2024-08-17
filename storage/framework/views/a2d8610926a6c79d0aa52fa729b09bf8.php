7ce5f414aca103c0bdd13c082970412622f7a7ad
dasundk <98402154+DasunDK@users.noreply.github.com>
2024-08-17 15:23:27 +0530

    product updae from image preview add

--- storage/framework/views/a2d8610926a6c79d0aa52fa729b09bf8.php
+++ storage/framework/views/a2d8610926a6c79d0aa52fa729b09bf8.php
@@ -10,6 +10,28 @@
                 </div><!-- end card header -->
 
 
+                <div class="card card-height-100">
+                    <div class="card-header align-items-center d-flex">
+                        <h4 class="card-title mb-0 flex-grow-1">Product Image</h4>
+                        <div class="flex-shrink-0">
+                            <div class="flex-shrink-0">
+                                <a href="<?php echo e(url('storage/' . $currentImg)); ?>" download="Product Image"
+                                    class="btn btn-primary"><i class=" ri-file-download-line align-bottom"></i>
+                                    Download</a>
+                            </div>
+                        </div>
+                    </div><!-- end card header -->
+
+                    <div class="card-body mx-auto">
+
+                        <a href="<?php echo e(url('storage/' . $currentImg)); ?>" data-fancybox>
+                            <img src="<?php echo e(url('storage/' . $currentImg)); ?>" class="img-thumbnail"
+                                style="height: 333px; width: 800px;" alt="Nic Front Image">
+                        </a>
+
+                    </div><!-- end card body -->
+                </div><!-- end card -->
+
                 <div class="card-body p-0 pb-2">
                     <div class="w-100 p-5">
 
--- storage/framework/views/a57160b985cb500cb4be1198b8b3da58.php
+++
@@ -1,5 +0,0 @@
-
-<body>
-
-<!-- <body data-layout="horizontal"> -->
-<?php /**PATH D:\MY Projects\WEB\laravel\Inventry_system\grama_sewaka_03\resources\views/layouts/body.blade.php ENDPATH**/ ?>
